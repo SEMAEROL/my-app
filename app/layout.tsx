49bb38a37c85e6bd24e07165c7a0a6e80400f3fd
semaerol <semaerol543@gmail.com>
2025-09-01 16:57:04 +0300

    loyout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <div className="min-h-screen flex flex-col">
           <Navbar />
-          {children}
+          <main className="flex-grow">{children}</main>
           <Footer />
         </div>         
       </body>
